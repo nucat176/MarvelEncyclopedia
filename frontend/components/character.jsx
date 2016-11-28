65c5f995d718a824b7720c9b91ca75f4f5b2d4be
Adam Foosaner <nucat176@gmail.com>
2016-11-28 11:23:03 -0800

    adds search

--- frontend/components/character.jsx
+++ frontend/components/character.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import {hashHistory} from 'react-router';
+import SearchBar from './search_bar';
 
 class Character extends React.Component {
   constructor(props){
     super(props);
     this.createImgLink = this.createImgLink.bind(this);
+    this.backToHome = this.backToHome.bind(this);
+    this.renderDescription = this.renderDescription.bind(this);
     this.state = {character: {}}
   }
 
@@ -21,11 +25,38 @@ class Character extends React.Component {
     }
   }
 
+  backToHome(){
+    hashHistory.push("/");
+  }
+
+  renderDescription(){
+    if (this.state.character.description === ""){
+      return (<span></span>);
+    } else {
+      return (
+        <p className="character-description">
+          {this.state.character.description}
+          <button className='back-to-home' onClick={this.backToHome}>Back To Home</button>
+        </p>
+      );
+    }
+  }
+
   render(){
     return (
-      <section>
-        <h1>{this.state.character.name}</h1>
-        <img className='character-thumbnail' src={this.createImgLink()}/>
+      <section className='characters-section'>
+        <div className='overlay'>
+          <div className='character-div'>
+            <h1 className='char-title'>
+              {this.state.character.name}
+              <SearchBar />
+            </h1>
+            <div className="char-info">
+              <img className='character-show-img' src={this.createImgLink()}/>
+              {this.renderDescription()}
+            </div>
+          </div>
+        </div>
       </section>
     );
   }
--- frontend/util/characters_api_util.js
+++ frontend/util/characters_api_util.js
@@ -25,3 +25,12 @@ export const fetchCharacter = (id, success, error) => {
     error
   });
 };
+
+export const searchCharacter = (query, success, error) => {
+  $.ajax({
+    type: "GET",
+    url: `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${query}&apikey=6eb0cc95d26564f272ab91b35d67b7b8`,
+    success,
+    error
+  });
+}
